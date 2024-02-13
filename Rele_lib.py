4a717f2cc2176e5ecaa21d384bc374d8dc236295
BenaTom <101045555+BenaTom@users.noreply.github.com>
2024-02-13 23:30:59 +0100

    Komentáře

--- Rele_lib.py
+++ Rele_lib.py
@@ -1,33 +1,33 @@
+# Zkusebni kod pro knihovnu ovladani rele
+# 3 typy rele
+# Autor: Benjamin Zezula
 
-
-
-
-class Relay(object):
+# parental class
+class Relay:
     
     def __init__(self, alias, state):
         self.alias = alias
         self.state = state
         self.type = "none"   
-
+    # zapnuti rele
     def start(self):
         if self.state != 1:
             self.state = 1
-
+    # vypnuti rele
     def stop(self):
         if self.state != 0:
             self.state = 0
-
+    # vraceni informace o stavu
     def get_state(self):
         return self.state
-    
+    # definovani typu rele
     def set_type(self, type):
         self.type = type    
-          
-    
+    # vraceni informaci o rele
     def get_info(self):
         return [self.alias, self.type]
     
-  
+
 class RelayNO(Relay):
     def __init__(self, alias, state):
         super().__init__(alias, state)
